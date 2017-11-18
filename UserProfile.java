4b4165ae3602cdce1f639591dc64377eb6fe8348
Durman2008 <tomdurman@ntlworld.com>
2017-11-18 20:33:17 +0000

    Added all getters & setters
    Updated order on some attributes, generated the getters & setters javadoc and added some additional info on them.

--- UserProfile.java
+++ UserProfile.java
@@ -12,8 +12,8 @@ public class UserProfile {
 	private String firstName;
 	private String lastName;
 	private String street;
-	private String cityTown;
 	private String postcode;
+	private String cityTown;
 	private Integer phoneNumber;
 	
 	/**
@@ -27,30 +27,156 @@ public class UserProfile {
 	 * @param phoneNumber The Users current phone number.
 	 */
 	public UserProfile(String username, String firstName, String lastName, String street,
-					   String cityTown, String postcode, Integer phoneNumber) {
+			 		   String postcode, String cityTown, Integer phoneNumber) {
 		
 		this.username = username;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.street = street;
-		this.cityTown = cityTown;
 		this.postcode = postcode;
+		this.cityTown = cityTown;
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * Method to get the userId.
+	 * @return the userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * Method to set a new userId.
+	 * @param userId the userId to set
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * Method to get the username.
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * Method to set a new username.
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Method to get the first name.
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Method to set a new first name.
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * Method to get the last name.
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * Method to set a new last name.
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * Method to get the street.
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * Method to set a new street address.
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * Method to get the postcode.
+	 * @return the postcode
+	 */
+	public String getPostcode() {
+		return postcode;
+	}
+
+	/**
+	 * Method to set a new postcode.
+	 * @param postcode the postcode to set
+	 */
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	/**
+	 * Method to get the city / town.
+	 * @return the cityTown
+	 */
+	public String getCityTown() {
+		return cityTown;
+	}
+
+	/**
+	 * Method to set a new city / town.
+	 * @param cityTown the cityTown to set
+	 */
+	public void setCityTown(String cityTown) {
+		this.cityTown = cityTown;
+	}
+
+	/**
+	 * Method to get the phone number.
+	 * @return the phoneNumber
+	 */
+	public Integer getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * Method to set a new phone number.
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(Integer phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
 	
-	/**
-	 * Method to set a new Username.
-	 * @param newUsername The new Username.
-	 */
-	public void setUsername(String newUsername) {
-		this.username = newUsername;
-	}
-	/**
-	 * Method to get Username.
-	 * @return The current Username.
-	 */
-	public String getUsername() {
-		return this.username;
-	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
