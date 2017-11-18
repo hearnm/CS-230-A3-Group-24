167cd0ee0f5187f8c81f4acfdf3d67491c4a0e75
Durman2008 <tomdurman@ntlworld.com>
2017-11-18 20:10:01 +0000

    Username setter & getter (+javadoc)
    Added a setter and getter for the username attribute, added some javadoc as well.

--- UserProfile.java
+++ UserProfile.java
@@ -16,6 +16,16 @@ public class UserProfile {
 	private String postcode;
 	private Integer phoneNumber;
 	
+	/**
+	 * Constructor for UserProfile which creates new Users.
+	 * @param username The username of the User.
+	 * @param firstName The first name of the User.
+	 * @param lastName The last name of the User.
+	 * @param street The Users current street address.
+	 * @param cityTown The Users city or town.
+	 * @param postcode The Users current postcode.
+	 * @param phoneNumber The Users current phone number.
+	 */
 	public UserProfile(String username, String firstName, String lastName, String street,
 					   String cityTown, String postcode, Integer phoneNumber) {
 		
@@ -28,6 +38,19 @@ public class UserProfile {
 		this.phoneNumber = phoneNumber;
 	}
 	
+	/**
+	 * Method to set a new Username.
+	 * @param newUsername The new Username.
+	 */
+	public void setUsername(String newUsername) {
+		this.username = newUsername;
+	}
+	/**
+	 * Method to get Username.
+	 * @return The current Username.
+	 */
+	public String getUsername() {
+		return this.username;
+	}
 	
 }
-
