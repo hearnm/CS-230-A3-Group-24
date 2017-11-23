6d22aad760585ea3e3ca9f067c92f54fe9c4e7fe
Tom <twdurman@gmail.com>
2017-11-23 16:28:57 +0000

    Added final attributes
    Added some final attributes, additional attributes

--- DrawImage.java
+++ DrawImage.java
@@ -7,7 +7,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,8 +14,16 @@ import javafx.stage.Stage;
 
 public class DrawImage extends Application {
 	
-	private Canvas canvas;
+	private static final int STAGE_WIDTH = 600;
+	private static final int STAGE_HEIGHT= 600;
+	private static final int CANVAS_WIDTH = 350;
+	private static final int CANVAS_HEIGHT = 350;
+	private static final int CIRCLE_WIDTH = 10;
+	private static final int CIRCLE_HEIGHT = 10;
 	
+	private Canvas canvas;
+	private double mouseX = 0.0;
+	private double mouseY = 0.0;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -26,7 +33,7 @@ public class DrawImage extends Application {
 		
 		Pane root = buildGUI();
 		
-		Scene scene = new Scene(root, 600, 600);
+		Scene scene = new Scene(root, STAGE_WIDTH, STAGE_HEIGHT);
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -36,7 +43,7 @@ public class DrawImage extends Application {
 
 		BorderPane root = new BorderPane();
 				
-		canvas = new Canvas(350, 350);
+		canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 		root.setCenter(canvas);
 		
 	    VBox sideBar = new VBox(200);
@@ -54,19 +61,12 @@ public class DrawImage extends Application {
 	    reset.setMaxWidth(Double.MAX_VALUE);
 	    
 	    root.setLeft(sideBar);
-	    
-		
-		
-		
-		
-		
-		
-		
+
 		canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				double mouseX = event.getX();
-				double mouseY = event.getY();
+				mouseX = event.getX();
+				mouseY = event.getY();
 				
 				GraphicsContext gc = canvas.getGraphicsContext2D();
 				
@@ -77,12 +77,12 @@ public class DrawImage extends Application {
 		canvas.setOnMouseDragged(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent event) {
-				double mouseX = event.getX();
-				double mouseY = event.getY();
+				mouseX = event.getX();
+				mouseY = event.getY();
 				
 				GraphicsContext gc = canvas.getGraphicsContext2D();
 				
-				gc.fillOval(mouseX, mouseY, 10, 10);
+				gc.fillOval(mouseX, mouseY, CIRCLE_WIDTH, CIRCLE_HEIGHT);
 			}
 		});	
 		
@@ -92,7 +92,7 @@ public class DrawImage extends Application {
 	public void resetCanvas() {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		
-		gc.clearRect(0,0,350,350);
+		gc.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
 	}
 	
 	
