ea030ea41da98b152fdccd1865cacdee6e39b4ff
Tom <twdurman@gmail.com>
2017-11-23 22:19:27 +0000

    Added open/close file stream methods
    Also added some comments

--- SessionData.java
+++ SessionData.java
@@ -1,8 +1,34 @@
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
 
+/**
+ * SessionData.java
+ * @author Tom Durman
+ * This class retrieves appropriate data and stores it in a Text file. Later calling that data from the same file.
+ */
 public class SessionData {
 
 	public SessionData() {
 		
 	}
+	
+	/**
+	 * 
+	 * @param filename Absolute or relative path to a file
+	 * @return The file path opened by filename
+	 */
+	public static File openFile(String filename){
+		File inputFile = new File(filename);
+		
+		return inputFile;
+	}
+	
+	/**
+	 * Method to close the file path
+	 * @param in the scanner of the file
+	 */
+	public static void closeFile(Scanner in) {
+		in.close();
+	}
+	
 }
