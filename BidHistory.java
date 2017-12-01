927e6a356dbb98b7c199f0c190f864cb9e82f93a
Tom <twdurman@gmail.com>
2017-12-01 19:12:42 +0000

    BidHistory updated (needs to be redone)

--- BidHistory.java
+++ BidHistory.java
@@ -1,70 +1,60 @@
-import java.util.*;
+import java.util.ArrayList;
 
 /**
  * BidHistory.java
  * @author James Shoemark-Smith
- * Class to view bidding history.
+ * Class to view two sets of bidding history (seller / buyer).
  */
 
 public class BidHistory {
 	
-	private int bidHistoryID;		//Variable for a unique ID
-	private int bidTime;			//Variable for the time of the bid
-	private String bidDate;			//Variable for the date of the bid
-	private String bidArtwork;		//Variable for the artwork in question
-	private String currentBidder;	//Variable for the active bidder
-	private double bidAmount;		//Variable for the current amount bid
+	private static int idCounter = 1; 	// Static variable used to set a unique ID
 	
-	public BidHistory(){ 			//Creation of the bidHistory constructor
+	private int bidHistoryID;			// Variable for a unique ID
+	private String bidDateTime;			// Variable for the time of the bid
+	private String artworkName;			// Variable for the name of the Artwork
+	private String currentBidder;		// Variable for the active bidder
+	private double bidAmount;			// Variable for the current amount bid
+	
+
+	/**
+	 * Constructor for Bid History.
+	 */
+	public BidHistory() { 	
+		this.bidHistoryID = idCounter;
+		idCounter++;
 	}
 
+	/**
+	 * Method to get the Id of the current Bid History.
+	 * @return bidHistory The Bid History Id
+	 */
 	public int getBidHistoryID() {
-		return bidHistoryID;
+		return this.bidHistoryID;
 	}
-
-	public void setBidHistoryID(int bidHistoryID) {
-		this.bidHistoryID = bidHistoryID;
+	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getPlacedBids() {
+		// This will return all of the placed bids (by the logged on user)
+		return null;
 	}
-
-	public int getBidTime() {
-		return bidTime;
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getReceivedBids() {
+		// This will return all of the received bids (by the logged on user)
+		return null;
 	}
-
-	public void setBidTime(int bidTime) {
-		this.bidTime = bidTime;
-	}
-
-	public String getBidDate() {
-		return bidDate;
-	}
-
-	public void setBidDate(String bidDate) {
-		this.bidDate = bidDate;
-	}
-
-	public String getBidArtwork() {
-		return bidArtwork;
-	}
-
-	public void setBidArtwork(String bidArtwork) {
-		this.bidArtwork = bidArtwork;
-	}
-
-	public String getCurrentBidder() {
-		return currentBidder;
-	}
-
-	public void setCurrentBidder(String currentBidder) {
-		this.currentBidder = currentBidder;
-	}
-
-	public double getBidAmount() {
-		return bidAmount;
-	}
-
-	public void setBidAmount(double bidAmount) {
-		this.bidAmount = bidAmount;
-	}
+	
+	
+	
 
 	
 }
