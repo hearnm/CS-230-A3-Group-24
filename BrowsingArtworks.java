8a36f5741f4031b3e3f4b892247414c172ef9ec1
Nikolina Antoniou <nikolina@Nikolinas-MacBook-Pro.local>
2017-12-01 01:37:26 +0000

    Created BrowsingArtworks class

--- BrowsingArtworks.java
+++ BrowsingArtworks.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +12,24 @@ import javafx.stage.Stage;
  */
 public class BrowsingArtworks extends Application {
 	
+	public ArrayList<Artwork> getArtworks() {
+		return artworks;
+	}
+
+	public void setArtworks(ArrayList<Artwork> artworks) {
+		this.artworks = artworks;
+	}
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	}
+
+	private ArrayList<Artwork> artworks;
+	private String search;
 	private static final int STAGE_WIDTH = 800;    //Width of the Stage
 	private static final int STAGE_HEIGHT = 750;   //Height of the Stage
 	
