e21be46dc97841beae3c01b363b7e376568de64c
Tom <twdurman@gmail.com>
2017-12-03 00:15:15 +0000

    Added Home Page to SystemGUI

--- SystemGUI.java
+++ SystemGUI.java
@@ -18,6 +18,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -77,6 +78,7 @@ public class SystemGUI extends Application {
 	private Stage window;			// The main stage, displaying the current Scene
 	private Scene login;			// The Scene to hold the login Page GUI
 	private Scene signUp;
+	private Scene home;
 	private Scene profile;			// The Scene to hold the Profile Page GUI
 	private Scene profileDrawImg;	// The Scene to hold the Profile Draw Image GUI
 	private Scene profileAvatars;	// The Scene to hold the Profile Default Avatars GUI
@@ -171,7 +173,10 @@ public class SystemGUI extends Application {
         	
         		UserProfile.setCurrentUserID(UserProfile.getCurrentUserId(usernameInput.getText()));
         		
-        		usernameInput.setText("Login Successful");
+        		Pane draw = buildHomePageGUI();
+        		home = new Scene(draw, MAIN_STAGE_WIDTH, MAIN_STAGE_HEIGHT);
+        		window.setScene(home);
+        		
         	} else {
         		usernameInput.setText("Login Unsuccessful");
         	}
@@ -350,7 +355,85 @@ public class SystemGUI extends Application {
 			return true;
 		}
 	}
+
+	
+	private Pane buildHomePageGUI(){
 		
+		BorderPane root = new BorderPane();
+		root.setStyle("-fx-background-color: linear-gradient(to bottom, #f2f2f2, #778899);");
+		
+		VBox top = new VBox();
+		VBox titleBar = new VBox();
+		HBox buttonBar = new HBox();
+		VBox bottom = new VBox();
+		HBox homepage = new HBox();
+		HBox bottomBar = new HBox();
+		
+		top.setSpacing(15);
+		top.setPadding(new Insets(50,20,20,0));
+		
+		buttonBar.setSpacing(170);
+		buttonBar.setPadding(new Insets(40,20,0,40));
+		
+		//Create elements that are needed for top VBox
+		Text title = new Text("Artatawe\n");
+		Text subTitle = new Text("Home Page");
+		Button auctionsButton = new Button("Auctions");
+		Button paintingsButton = new Button("Paintings");
+		Button sculpturesButton = new Button("Sculptures");
+		
+		auctionsButton.setMinWidth(70);
+		paintingsButton.setMinWidth(70);
+		sculpturesButton.setMinWidth(80);
+		
+		auctionsButton.setMaxWidth(Double.MAX_VALUE);
+		paintingsButton.setMaxWidth(Double.MAX_VALUE);
+		sculpturesButton.setMaxWidth(Double.MAX_VALUE);
+		
+		auctionsButton.setPrefWidth(1500);
+		paintingsButton.setPrefWidth(1500);
+		sculpturesButton.setPrefWidth(1500);
+		
+		
+		//Postion title text
+		title.setScaleX(4);
+		title.setScaleY(4);
+		subTitle.setScaleX(2.5);
+		subTitle.setScaleY(2.5);
+		title.setTextAlignment(TextAlignment.LEFT);
+		
+		//Position buttons
+		auctionsButton.resize(87,80);
+		paintingsButton.resize(87, 20);
+		sculpturesButton.resize(87, 20);
+		
+		//Create elements that are needed for bottom VBox
+		ScrollPane scroll = new ScrollPane();
+		Button logOut = new Button("Log out");
+		
+		/*
+		for(Artwork a : Artwork){
+			homepage.getChildren().add(a.getTitle(),a.getDescription());
+		}
+		*/
+		
+		
+		//Position button
+		logOut.resize(87,20);
+		
+		//Add elements to the top VBox
+		buttonBar.getChildren().addAll(auctionsButton,paintingsButton,sculpturesButton);
+		titleBar.setAlignment(Pos.BASELINE_CENTER);
+		titleBar.getChildren().addAll(title, subTitle);
+		top.getChildren().addAll(titleBar,buttonBar);
+		
+		//Add elements to the bottom VBox
+		
+		root.setTop(top);
+		
+		
+		return root;
+	}
 	
 	
 	/**
@@ -446,8 +529,6 @@ public class SystemGUI extends Application {
 		}
 	}
 	
-	
-
 	/**
 	 * Method to build the Profile Draw Image GUI window
 	 * @return root The Constructed Pane with all the Profile Draw Image GUI elements
