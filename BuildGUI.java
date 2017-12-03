b90f911d34b32e598d2efb631f628a3a68f2b7e7
Nikolina Antoniou <nikolina@Nikolinas-MacBook-Pro.local>
2017-12-03 05:03:22 +0000

    Able to switch scenes between homepage and the chosen one

--- BuildGUI.java
+++ BuildGUI.java
@@ -1,15 +1,13 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -19,28 +17,30 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class BuildGUI extends Application {
-
+	Stage window;
+	//Scene auctionScene,paintingScene,sculptureScene;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
-		Pane root = buildSearchArtworkGUI();
-		Scene scene = new Scene(root,700,500);
-		primaryStage.setTitle("Artatawe Application");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		window = primaryStage;
+		
+		Scene scene = getHomeScene();
+		window.setTitle("Artatawe Application");
+		window.setScene(scene);
+		window.show();
 	}
 
-	private Pane buildSearchArtworkGUI(){
+	private Scene getHomeScene(){
 		
 		BorderPane root = new BorderPane();
 		root.setStyle("-fx-background-color: linear-gradient(to bottom, #f2f2f2, #778899);");
 		
 		VBox top = new VBox();
-		VBox titleBar = new VBox();
+		HBox titleBar = new HBox();
 		HBox buttonBar = new HBox();
 		VBox bottom = new VBox();
 		HBox homepage = new HBox();
@@ -53,62 +53,168 @@ public class BuildGUI extends Application {
 		buttonBar.setPadding(new Insets(40,20,0,40));
 		
 		//Create elements that are needed for top VBox
-		Text title = new Text("Artatawe\n");
-		Text subTitle = new Text("Home Page");
-		Button auctionsButton = new Button("Auctions");
-		Button paintingsButton = new Button("Paintings");
-		Button sculpturesButton = new Button("Sculptures");
-		
-		auctionsButton.setMinWidth(70);
-		paintingsButton.setMinWidth(70);
-		sculpturesButton.setMinWidth(80);
-		
-		auctionsButton.setMaxWidth(Double.MAX_VALUE);
-		paintingsButton.setMaxWidth(Double.MAX_VALUE);
-		sculpturesButton.setMaxWidth(Double.MAX_VALUE);
-		
-		auctionsButton.setPrefWidth(1500);
-		paintingsButton.setPrefWidth(1500);
-		sculpturesButton.setPrefWidth(1500);
-		
+		Text title = new Text("Artatawe");
+		Button auctions = new Button("Auctions");
+		Button paintings = new Button("Paintings");
+		Button sculptures = new Button("Sculptures");
 		
 		//Postion title text
 		title.setScaleX(4);
 		title.setScaleY(4);
-		subTitle.setScaleX(2.5);
-		subTitle.setScaleY(2.5);
-		title.setTextAlignment(TextAlignment.LEFT);
+		title.setTextAlignment(TextAlignment.CENTER);
 		
-		//Position buttons
-		auctionsButton.resize(87,80);
-		paintingsButton.resize(87, 20);
-		sculpturesButton.resize(87, 20);
+		//Resize buttons
+		auctions.resize(87,20);
+		paintings.resize(87, 20);
+		sculptures.resize(87, 20);
 		
+		//Set action on buttons
+		auctions.setOnAction(e -> window.setScene(getAuctionScene()));
+		paintings.setOnAction(e -> window.setScene(getPaintingScene()));
+		sculptures.setOnAction(e -> window.setScene(getSculptureScene()));
+				
 		//Create elements that are needed for bottom VBox
 		ScrollPane scroll = new ScrollPane();
 		Button logOut = new Button("Log out");
 		
-		/*
-		for(Artwork a : Artwork){
-			homepage.getChildren().add(a.getTitle(),a.getDescription());
-		}
-		*/
-		
-		
-		//Position button
+		//Resize button
 		logOut.resize(87,20);
 		
 		//Add elements to the top VBox
-		buttonBar.getChildren().addAll(auctionsButton,paintingsButton,sculpturesButton);
+		buttonBar.getChildren().addAll(auctions,paintings,sculptures);
 		titleBar.setAlignment(Pos.BASELINE_CENTER);
-		titleBar.getChildren().addAll(title, subTitle);
+		titleBar.getChildren().add(title);
 		top.getChildren().addAll(titleBar,buttonBar);
 		
 		//Add elements to the bottom VBox
-		
+	
+		//Set root
 		root.setTop(top);
 		
-		
-		return root;
+		Scene scene = new Scene(root,700,500);
+		return scene;
 	}
+	private Scene getAuctionScene() {
+
+		BorderPane root = new BorderPane();
+		root.setStyle("-fx-background-color: linear-gradient(to bottom, #f2f2f2, #778899);");
+
+		VBox top = new VBox();
+		VBox titleBar = new VBox();
+		HBox buttonBar = new HBox();
+		VBox bottom = new VBox();
+		HBox bottomBar = new HBox();
+		
+		top.setSpacing(15);
+		top.setPadding(new Insets(50,20,20,0));
+
+		buttonBar.setSpacing(170);
+		buttonBar.setPadding(new Insets(40,20,0,40));
+		
+		//Create elements that are needed for top VBox
+		Text title = new Text("Auctions");
+		Button back = new Button("Home");
+		
+		back.setOnAction(e -> window.setScene(getHomeScene()));
+		
+		//Position title text 
+		title.setScaleX(4);
+		title.setScaleY(4);
+		title.setTextAlignment(TextAlignment.CENTER);
+		
+		//Resize buttons
+		back.resize(87,80);
+		
+		buttonBar.getChildren().add(back);
+		titleBar.setAlignment(Pos.BASELINE_CENTER);
+		titleBar.getChildren().add(title);
+		top.getChildren().addAll(titleBar,buttonBar);
+		root.setTop(top);
+		Scene scene = new Scene(root,700,500);
+		return scene;
+	}
+	
+	private Scene getPaintingScene(){
+		
+		BorderPane root = new BorderPane();
+		root.setStyle("-fx-background-color: linear-gradient(to bottom, #f2f2f2, #778899);");
+
+		VBox top = new VBox();
+		VBox titleBar = new VBox();
+		HBox buttonBar = new HBox();
+		VBox bottom = new VBox();
+		HBox bottomBar = new HBox();
+		
+		top.setSpacing(15);
+		top.setPadding(new Insets(50,20,20,0));
+
+		buttonBar.setSpacing(170);
+		buttonBar.setPadding(new Insets(40,20,0,40));
+		
+		//Create elements that are needed for top VBox
+		Text title = new Text("Paintings");
+		Button back = new Button("Home");
+		
+		back.setOnAction(e -> window.setScene(getHomeScene()));
+		
+		//Position title text 
+		title.setScaleX(4);
+		title.setScaleY(4);
+		title.setTextAlignment(TextAlignment.CENTER);
+		
+		//Resize buttons
+		back.resize(87,80);
+		
+		buttonBar.getChildren().add(back);
+		titleBar.setAlignment(Pos.BASELINE_CENTER);
+		titleBar.getChildren().add(title);
+		top.getChildren().addAll(titleBar,buttonBar);
+		root.setTop(top);
+		Scene scene = new Scene(root,700,500);
+		return scene;
+	}
+	
+	private Scene getSculptureScene(){
+		
+		BorderPane root = new BorderPane();
+		root.setStyle("-fx-background-color: linear-gradient(to bottom, #f2f2f2, #778899);");
+
+		VBox top = new VBox();
+		VBox titleBar = new VBox();
+		HBox buttonBar = new HBox();
+		VBox bottom = new VBox();
+		HBox bottomBar = new HBox();
+		
+		top.setSpacing(15);
+		top.setPadding(new Insets(50,20,20,0));
+
+		buttonBar.setSpacing(170);
+		buttonBar.setPadding(new Insets(40,20,0,40));
+		
+		//Create elements that are needed for top VBox
+		Text title = new Text("Sculptures");
+		Button back = new Button("Home");
+		
+		back.setOnAction(e -> window.setScene(getHomeScene()));
+		
+		//Position title text 
+		title.setScaleX(4);
+		title.setScaleY(4);
+		title.setTextAlignment(TextAlignment.CENTER);
+		
+		//Resize buttons
+		back.resize(87,80);
+		
+		buttonBar.getChildren().add(back);
+		titleBar.setAlignment(Pos.BASELINE_CENTER);
+		titleBar.getChildren().add(title);
+		top.getChildren().addAll(titleBar,buttonBar);
+		
+		root.setTop(top);
+		Scene scene = new Scene(root,700,500);
+		return scene;
+	}
+	
+
 }
+
