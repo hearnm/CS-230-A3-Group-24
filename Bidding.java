436a7c8343b7affb3513bba30f6d0d575924566b
Emily Fothergill <915456@swansea.ac.uk>
2017-12-06 17:02:33 +0000

    Added ArrayList

--- Bidding.java
+++ Bidding.java
@@ -1,9 +1,9 @@
-
+import java.util.ArrayList;
 public class Bidding {
 
 	private String username;
 	private double bidAmount;
-	
+	private ArrayList<Bidding> bids = new ArrayList<Bidding>();
 	
 	public Bidding() {
 		
