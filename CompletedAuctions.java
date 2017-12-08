16b901951bcecd4f843541f2bc291c7eb5cd2eeb
Emily Fothergill <915456@swansea.ac.uk>
2017-12-08 23:05:03 +0000

    javadoc updated

--- CompletedAuctions.java
+++ CompletedAuctions.java
@@ -1,34 +1,46 @@
 import java.util.ArrayList;
 
 /**
- * @author nikolina
- * Shows a list with all the artworks which have successfully been auctioned.
+ * CompletedAuctions.java
+ * Shows a list with all the Artwork which have successfully been auctioned.
+ * @author Nikolina Antoniou
  */
 public class CompletedAuctions {
+	/** The list of artworks. */
 	private ArrayList<Artwork> listOfArtworks;
+
+	/** The artwork name. */
 	private String artworkName;
+	/** The winning bid. */
 	private double winningBid;
+	/** The winning bidder. */
 	private String winningBidder;
-	
-	public CompletedAuctions(){
-		
+
+	/**
+	 * Instantiates a new completed Auction.
+	 */
+	public CompletedAuctions() {
+
 	}
 
 	/**
-	 * @return a list of artworks
+	 * Gets the list of Artwork.
+	 * @return a list of Artwork
 	 */
 	public ArrayList<Artwork> getListOfArtworks() {
 		return listOfArtworks;
 	}
 
 	/**
-	 * @param listOfArtworks
+	 * Sets the list of Artwork.
+	 * @param listOfArtworks the new list of Artwork
 	 */
 	public void setListOfArtworks(ArrayList<Artwork> listOfArtworks) {
 		this.listOfArtworks = listOfArtworks;
 	}
 
 	/**
+	 * Gets the artwork name.
 	 * @return the name of the artwork
 	 */
 	public String getArtworkName() {
@@ -36,13 +48,15 @@ public class CompletedAuctions {
 	}
 
 	/**
-	 * @param artworkName
+	 * Sets the artwork name.
+	 * @param artworkName the new artwork name
 	 */
 	public void setArtworkName(String artworkName) {
 		this.artworkName = artworkName;
 	}
 
 	/**
+	 * Gets the winning bid.
 	 * @return the winning bid
 	 */
 	public double getWinningBid() {
@@ -50,13 +64,15 @@ public class CompletedAuctions {
 	}
 
 	/**
-	 * @param winningBid
+	 * Sets the winning bid.
+	 * @param winningBid the new winning bid
 	 */
 	public void setWinningBid(double winningBid) {
-		this.winningBid = winningBid; 
+		this.winningBid = winningBid;
 	}
 
 	/**
+	 * Gets the winning bidder.
 	 * @return the winning bidder
 	 */
 	public String getWinningBidder() {
@@ -64,11 +80,10 @@ public class CompletedAuctions {
 	}
 
 	/**
-	 * @param winningBidder
+	 * Sets the winning bidder.
+	 * @param winningBidder the new winning bidder
 	 */
 	public void setWinningBidder(String winningBidder) {
 		this.winningBidder = winningBidder;
 	}
-	
-
 }
